798e52ed57a145b1a8ebe33d884229c370cb90ca
ddev187 <yawnhenley@gmail.com>
2022-10-10 07:08:24 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+//1) MIDDLEWARE
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//3) ROUTES
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+//4) START SERVER
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
